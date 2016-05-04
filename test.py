904e5ef25b811c242d000741fd786f1041576a17
Sirui Feng <siruif@uchicago.edu>
2016-05-03 21:13:08 -0500

    training set

---
+++ test.py
@@ -0,0 +1,15 @@
+import json
+public_utilities_path = 'data/public_utilities.json'
+with open(public_utilities_path) as datafile:
+	b_set =set()
+	rv = list()
+
+	for line in datafile:
+		row = json.loads(line)
+		business_id = row["business_id"]
+		if business_id not in b_set:
+			rv.append(row)
+			b_set.add(business_id)
+with open('data/training.txt','w') as f:
+	for each in rv:
+		print(each, file = f)
