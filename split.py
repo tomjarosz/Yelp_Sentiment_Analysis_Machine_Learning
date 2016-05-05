af7cc8ec049ece32b12d4069fd6a4cadbd58df97
Sirui Feng <siruif@uchicago.edu>
2016-05-05 10:26:07 -0500

    training data

--- split.py
+++ split.py
@@ -9,6 +9,8 @@ This file splits each review on periods and conjuctions.
 import re
 import json
 from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+import csv
 
 public_utilities_path = 'data/public_utilities.json'
 
@@ -44,4 +46,32 @@ def go():
 						print("score:", sentence.sentiment.polarity)
 						print()
 			break
-go()
+
+def get_training():
+	with open(public_utilities_path) as datafile:
+		with open('data/training_data.csv', 'w') as outfile:
+			writer = csv.DictWriter(outfile, fieldnames = ['review_id', \
+				'business_id', 'user_id', 'stars', 'blob_polarity', 'review', \
+				'label'])
+			writer.writeheader()
+			i=0
+
+			for line in datafile:
+				i+=1
+				print(i)
+				row = json.loads(line)
+				review = row['text']
+				review = review.lower()
+
+				#split only on periods
+				sentences = split_period(review)
+				for s in sentences:
+					blob = TextBlob(s, analyzer = NaiveBayesAnalyzer())
+					polarity = blob.polarity
+
+					writer.writerow({'review_id':row['review_id'], \
+						'business_id': row['business_id'], \
+						'user_id':row['user_id'], 'stars':row['stars'], \
+						'blob_polarity': polarity, 'review': s})
+
+get_training()
