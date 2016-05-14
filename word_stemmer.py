6e34871ce42b59aa0a5141da17f60a06c8ee0fe7
Tom Jarosz <tjarosz@uchicago.edu>
2016-05-14 15:56:38 -0500

    word stemmer

---
+++ word_stemmer.py
@@ -0,0 +1,8 @@
+from nltk.stem.porter import *
+
+def word_stemmer(sentence):
+
+    for word in sentence.split(" "):
+        PorterStemmer().stem_word(word)
+        stem_sentence = " ".join(PorterStemmer().stem_word(word) for word in sentence.split(" "))
+    return stem_sentence
