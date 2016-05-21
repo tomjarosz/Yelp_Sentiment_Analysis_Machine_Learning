98092ded269d177c4ba676700661e949f4f18401
Vi Nguyen <Vi Nguyen>
2016-05-21 13:52:56 -0500

    NB with keywrd list

--- model.py
+++ model.py
@@ -19,12 +19,18 @@ from sklearn import (preprocessing, cross_validation, svm, metrics, tree,
 
 import csv
 
-labels = ['complaints', 'compliments', 'neutral', 'suggestions for user', 'suggestion for busn']
+labels = ['complaints', 'compliments', 'suggestions for user', 'suggestion for busn']
+models_dict = {}
 
 complaint_kwords = list(set(open("data/word_list/complaints.txt").read().splitlines()))
 compliments_kwords = list(set(open("data/word_list/compliments.txt").read().splitlines()))
 suggestions_busn_kwords = list(set(open("data/word_list/suggestion_busn.txt").read().splitlines()))
 suggestions_user_kwords = list(set(open("data/word_list/suggestion_user.txt").read().splitlines()))
+
+models_dict['complaints'] = complaint_kwords
+models_dict['compliments'] = complaint_kwords
+models_dict['suggestions for user'] = suggestions_user_kwords
+models_dict['suggestion for busn'] = suggestions_user_kwords
 
 
 def read_csv_to_df(filename):
@@ -34,51 +40,56 @@ def read_csv_to_df(filename):
 	return df
 
 
-def train_test_set(df, y_label):
+def train_test(df, y_label):
 	if y_label in labels:
 		labels.remove(y_label)
 	df_dropped_other_labeled = df.drop(labels, axis = 1)	#exclude all labeled columns but label in focus
 	train, test = train_test_split(df_dropped_other_labeled, test_size = 0.2, random_state = 0)
 
 	#baseline accuracy
-	print('baseline_accuracy overall: \n {}'.format(df.groupby(y_label).size()))
-	print('baseline_accuracy train: \n {}'.format(train.groupby(y_label).size()))
-	print('baseline_accuracy test: \n {}'.format(test.groupby(y_label).size()))
+	#print('baseline_accuracy overall: \n {}'.format(df.groupby(y_label).size()))
+	#print('baseline_accuracy train: \n {}'.format(train.groupby(y_label).size()))
+	print('baseline_accuracy test: \n {}'.format(1 - test[y_label].mean()))
 	
-	#return train, test
+	return train, test
 
 
-def naivebayes_model(train, test, y_value):
-	cv = CountVectorizer(vocabulary = complaint_kwords)
-	array = cv.fit_transform(list(train['review'])).toarray()
+def naivebayes_model(train, test, y_label, word_list, polarity_inc = True):
+	cv = CountVectorizer(vocabulary = word_list)
+	train_x_vector = cv.fit_transform(list(train['review'])).toarray()
+	test_x_vector = cv.fit_transform(list(test['review'])).toarray()
 
-	cv.vocabulary_['polarity'] = len(cv.vocabulary_) - 1 # get column names
 
-	# adding polarity as feature
-	polarity = np.asarray(train['blob_polarity'])
-	polarity = np.reshape(polarity, (len(polarity), 1))
+	if polarity_inc:
+		cv.vocabulary_['polarity'] = len(cv.vocabulary_) - 1 # get column names
 
-	x_vector = np.append(array, polarity, axis = 1)
-	y_values = np.asarray(train[y_value])
+		# adding polarity as feature
+		train_polarity = np.asarray(train['blob_polarity'])
+		train_polarity = np.reshape(train_polarity, (len(train_polarity), 1))
+		train_x_vector = np.append(train_x_vector, train_polarity, axis = 1)
+		
+		test_polarity = np.asarray(test['blob_polarity'])
+		test_polarity = np.reshape(test_polarity, (len(test_polarity), 1))
+		test_x_vector = np.append(test_x_vector, test_polarity, axis = 1)
+	
+	train_y_values = np.asarray(train[y_label])
 
 	clf = GaussianNB()
-	clf.fit(x_vector, y_values)
+	clf.fit(train_x_vector, train_y_values)
 
 	# prediction
-	test_x_vector = cv.fit_transform(list(test['review'])).toarray()
-	polarity = np.asarray(test['blob_polarity'])
-	polarity = np.reshape(polarity, (len(polarity), 1))
-
-	test_x_vector = np.append(array, polarity, axis = 1)
-	test_y_values = np.asarray(test[y_value])
+	test_y_values = np.asarray(test[y_label])
 
 	y_pred = clf.predict(test_x_vector)
 
-	print(metrics.accuracy_score(y_true, y_pred))
+	print(metrics.accuracy_score(test_y_values, y_pred))
 	
 
 if __name__ == '__main__':
 
 	df = read_csv_to_df("data/labeled_overlap_data.csv")
-	train, test = train_test_split(df, 'complaints')
-	array = naivebayes_model(train)
+
+	for key in models_dict:
+		print(key)
+		train, test = train_test(df, key)
+		naivebayes_model(train, test, key, models_dict[key])
