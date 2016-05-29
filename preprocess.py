327c552ed2bdfcb4efcabd05a060655605558d7c
Sirui Feng <siruif@uchicago.edu>
2016-05-29 15:12:04 -0500

    kfcv

--- preprocess.py
+++ preprocess.py
@@ -11,7 +11,6 @@ from sklearn.decomposition import PCA
 from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn import metrics
-
 from word_stemmer import word_stemmer
 import csv
 import numpy as np
@@ -167,12 +166,35 @@ def NaiveBayesClf(X_train, X_test, y_train, y_test):
 
 # 		print(metrics.accuracy_score(Y_true, Y_predict))
 
-def cross_validation(df):
-	X = df['stem_review']
-	labels = ['complaint', 'compliments', 'suggestion for user', 'suggestion for business']
-	for label in labels:
-		y = df[label]
-		k_fold = KFold(df.shape[0], n_folds = 6)
+def cross_validation(clf, X, y):
+	k = 7
+
+	evaludation_dict = dict()
+
+	scores = cross_validation.cross_val_score(clf, X, y_true, cv=k)
+	print ("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+	y_predicted = cross_validation.cross_val_predict(clf, X, y, cv=k)
+
+	evaludation_dict["accuracy"] = metrics.accuracy_score(y_true, y_predicted)
+	evaludation_dict["precision"] = metrics.precision_score(y_true, y_predicted)
+	evaludation_dict["roc_auc"] = metrics.roc_auc_score(y_true, y_predicted)
+
+
+	return evaludation_dict
+
+def classify(best_clf, X_train, y_train, X_full):
+	best_clf.fit(X_train, y_train)
+	y_full = get_predictions(best_clf, X_train, Y_train, X_full)
+	return y_full
+
+def output_full_to_dict(df_full):
+
+	df_full_predicted = df_full
+
+	return
+
+
 
 if __name__ == '__main__':
 
