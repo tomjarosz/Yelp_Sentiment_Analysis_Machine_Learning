a30c0627974260adcf0d2a7a9325d32620ca8f27
Turab Hassan <turabhassan@cs.uchicago.edu>
2016-06-03 20:35:56 -0500

    adding the web interface

---
+++ mlinterface/textinsighters/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import BusinessName
+# Create your views here.
+
+
+def suggestion(request):
+    
+    #print('show first time')
+    if request.method == 'POST':
+        print('getting to POST')
+        form = BusinessName(request.POST)
+        print(form)
+        if form.is_valid():
+            #print('this should show')
+            data=form.cleaned_data  
+            print(data['business_name'])
+            #context = insert_function_here(data)
+            form = BusinessName()
+            context = {'form':form}
+            return render( request,'mainpage.html', context)
+    else:
+        form = BusinessName()  
+        context = {'form':form}
+        print('first time')
+        print(form)
+        return render( request,'mainpage.html', context)
